6b0c68cc0fcccdf4428985269cd7a1151b24c06e
Razameer.Ahmed <razameer.ahmed@gmail.com>
2025-08-24 20:15:31 +0500

    initial API code .Net Core (.Net 5.0)

---
+++ UserService/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Data;
+using UserService.Models;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _context.Users;
+        }
+    }
+}
