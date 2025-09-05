aba43480d3e15adfbe59c7c95d09921449436921
syedkhalidhalim <syedkhalidhalim@hotmail.com>
2025-09-05 14:23:36 +0500

    bin and obj folder deleted and added to git ignore

--- Database/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ Database/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e34dfbc6d18f4f7df290321ea8c19e0207dabcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293c0bf2aac311062c36f279fa29e91d426203b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
